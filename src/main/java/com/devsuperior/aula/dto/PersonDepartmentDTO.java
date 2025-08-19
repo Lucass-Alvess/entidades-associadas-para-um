0cf1f2ef2d5b065c4457b0d14d901ba7bfe94ab1
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2025-08-19 19:03:33 -0300

    Save PersonDTO

--- src/main/java/com/devsuperior/aula/dto/PersonDepartmentDTO.java
+++ src/main/java/com/devsuperior/aula/dto/PersonDepartmentDTO.java
@@ -7,6 +7,7 @@ public class PersonDepartmentDTO {
     private Long id;
     private String name;
     private Double salary;
+
     private DepartmentDTO department;
 
     public PersonDepartmentDTO() {
